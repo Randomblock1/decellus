3513656b5080905a1f382a7d33fda05e6668097f
Benjamin G <therealrandomblock1@gmail.com>
2021-01-12 17:22:04 -0600

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,8 @@
+// runs when called by background.js
+
+(function() {
+
+	location.href="javascript:alert('yeeted lesson'); void 0";
+        location.href="javascript:loadFunction('', 'TRUE', '', '', ''); void 0";
+
+})();
